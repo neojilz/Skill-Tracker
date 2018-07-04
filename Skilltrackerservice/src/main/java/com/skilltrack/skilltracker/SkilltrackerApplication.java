4d5abfe2efebc3a6302b6f8c55f7c315811b4b4d
Jijil <jijil.kakkadathu@cognizant.com>
2018-07-04 14:47:29 +0530

    main file chaneg

--- Skilltrackerservice/src/main/java/com/skilltrack/skilltracker/SkilltrackerApplication.java
+++ Skilltrackerservice/src/main/java/com/skilltrack/skilltracker/SkilltrackerApplication.java
@@ -1,12 +1,20 @@
 package com.skilltrack.skilltracker;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class SkilltrackerApplication {
+public class SkilltrackerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SkilltrackerApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(
+			SpringApplicationBuilder application) {
+		return application.sources(SkilltrackerApplication.class);
 	}
 }
